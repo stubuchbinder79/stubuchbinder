75ff5c4f3d4c1e4782a3ae9cebf67a410065e66b
Stu Buchbinder <stu.buchbinder@redbox.com>
2020-01-16 09:28:55 -0600

    home page grid spacing updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,17 +9,8 @@ function HomePage() {
     return <Square key={project.id} square={project} />;
   });
   return (
-    <Grid container spacing={0}>
-      <div
-        style={{
-          backgroundImage: "url(../images.ticks.png)",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat"
-        }}
-      >
-        {projects}
-      </div>
+    <Grid container spacing={3}>
+      {projects}
     </Grid>
   );
 }
